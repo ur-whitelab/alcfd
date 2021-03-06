9aed13ae94a9acf7f2169835e61c6c9c96d59cb5
Mehrad Ansari <mehrad.ansari@rochester.edu>
2021-03-05 19:37:44 -0500

    validated model using HP eq, switched to dp/L

--- utilities.py
+++ utilities.py
@@ -5,8 +5,11 @@ import sys
 import numpy as np
 
 
-def run_model(pipe_D, bend_angle, rho, muo, inlet_v, inlet_p):
-        input_dict = {'D': pipe_D, 'Angle': bend_angle, 'Density': rho,
+def run_model(pipe_D, bend_angle, rho, muo, inlet_v, inlet_p, l_d_ratio=None):
+        if l_d_ratio is None:
+            l_d_ratio = 10
+            print('No input was given for pipe L/D. Assuming L/D = 10.')
+        input_dict = {'D': pipe_D, 'L_D_ratio': l_d_ratio, 'Angle': bend_angle, 'Density': rho,
                 'Viscosity': muo, 'V_in': inlet_v, 'P_in': inlet_p}
         df = pd.DataFrame(input_dict)
         df.to_csv('inputs.txt', header=True, sep='\t', index=None)
@@ -20,6 +23,8 @@ def run_model(pipe_D, bend_angle, rho, muo, inlet_v, inlet_p):
                 print('Failed to launch ANSYS Workbench!')
                 sys.exit(0)
         outputs = pd.read_csv('outputs.txt', delimiter='\t')
+        outputs = outputs.drop(columns=['Mesh max size',
+                                  'Pipe R', 'Pipe L', 'p0', 'p1'])
         print('Results are stored in outputs.txt')
         return outputs
 
@@ -28,12 +33,12 @@ def check_laminar(pipe_D, rho, inlet_v, muo):
     re_number = rho*inlet_v*pipe_D/muo
     a = np.where(re_number > 2100)[0]
     assert len(
-        a) == 0, f'Model input {a} parameters will result a turbulent flow. Revise parameters!'
+        a) == 0, f'Model input {a} parameters will result a turbulent flow. Revise parameters!\n Unacceptable Re = {re_number[a]}'
     return re_number
 
-def compute_Hagen_Poiseuille_del_P(muo, l, inlet_v, pipe_D):
-    del_p = 32 * muo * l * inlet_v / pipe_D**2
-    return del_p
+def compute_Hagen_Poiseuille_del_PL(muo, inlet_v, pipe_D):
+    del_pl = 32 * muo  * inlet_v / pipe_D**2
+    return del_pl
 
 
 # def execute_subprocess(cmd):
