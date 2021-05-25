5ddac371551c02cb7a5ed41f5a1ef8ee69242c23
Mehrad Ansari <mehrad.ansari@rochester.edu>
2021-05-25 00:29:48 -0400

    parallelized script added

--- utilities.py
+++ utilities.py
@@ -6,7 +6,7 @@ import numpy as np
 import shutil
 
 
-def run_model(pipe_D, bend_angle, inlet_v, rho=1.225, muo=1.7894e-5, inlet_p=101325, l_d_ratio=None, name=None, debug=False):
+def run_model(pipe_D, bend_angle, inlet_v, rho=1.225, muo=1.7894e-5, inlet_p=101325, l_d_ratio=None, name=None, debug=False, run_parallel=False):
     if l_d_ratio is None:
         l_d_ratio = 10
         print(f'No input was given for pipe L/D. Assuming L/D = {l_d_ratio}.')
@@ -35,14 +35,30 @@ def run_model(pipe_D, bend_angle, inlet_v, rho=1.225, muo=1.7894e-5, inlet_p=101
         df.to_csv(inputs_dir, header=True, sep='\t', index=None)
         print(f'Model inputs are saved in:\n {inputs_dir}') 
         # re_number, a = check_laminar(pipe_D, rho, inlet_v, muo)
-        script = '/scratch/awhite38_lab/cfdsr/alcfd/archived_models/elbowed_pipe/script.wbjn'
         wb = '/scratch/dfoster_lab/ansys2020R2/v202/Framework/bin/Linux64/runwb2'
-        shutil.copyfile(
-            inputs_dir, '/scratch/awhite38_lab/cfdsr/alcfd/archived_models/elbowed_pipe/inputs.txt')
-        if debug:
-            cmdline = '{} -B -R {} > run_log.txt'.format(wb, script)
+        script = '/scratch/awhite38_lab/cfdsr/alcfd/archived_models/elbowed_pipe/script.wbjn'
+        if not run_parallel:
+            shutil.copyfile(
+                inputs_dir, '/scratch/awhite38_lab/cfdsr/alcfd/archived_models/elbowed_pipe/inputs.txt')
         else:
-            cmdline = '{} -B -R {}'.format(wb, script)
+            ProjectPath = f'/scratch/awhite38_lab/cfdsr/alcfd/archived_models/elbowed_pipe/elbowed_pipe_{name}.wbpj'
+            input_file = inputs_dir
+            with open(script, "r") as f:
+                lines = f.readlines()
+                for m, line in enumerate(lines):
+                    if line.startswith('input_file'):
+                        lines[m] = f'input_file = "{input_file}"\n'
+                    if line.startswith('    ProjectPath'):
+                        lines[m] = f'    ProjectPath="{ProjectPath}",\n'
+            script_parallel = f'/scratch/awhite38_lab/cfdsr/alcfd/archived_models/elbowed_pipe/script_parallel_{name}.wbjn'
+            with open(script_parallel, "w") as f:
+                lines = "".join(lines)
+                f.write(lines)
+            script = script_parallel
+        if debug:
+            cmdline = f'{wb} -B -R {script} > run_log_{name}.txt'
+        else:
+            cmdline = f'{wb} -B -R {script}'
         try:
                 os.system(cmdline)
         except Exception:
@@ -51,6 +67,12 @@ def run_model(pipe_D, bend_angle, inlet_v, rho=1.225, muo=1.7894e-5, inlet_p=101
         outputs = pd.read_csv(outputs_dir, delimiter='\t')
         outputs = outputs.drop(columns=['Mesh max size', 'Pipe R', 'Pipe L', 'p0', 'p1'])
         print(f'Results are stored in:\n {outputs_dir}.')
+        if run_parallel:
+            shutil.rmtree(
+                f'/scratch/awhite38_lab/cfdsr/alcfd/archived_models/elbowed_pipe/elbowed_pipe_{name}_files')
+            os.remove(
+                f'/scratch/awhite38_lab/cfdsr/alcfd/archived_models/elbowed_pipe/elbowed_pipe_{name}.wbpj')
+            os.remove(script)
         return outputs
     else: 
         print('Model cannot run. All inputs resulted a turbulent flow.')
